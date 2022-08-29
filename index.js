9cb26b4fc3aad36a780d837e3fd28b7274684fcf
rawbin <57212166+raawbin@users.noreply.github.com>
2022-08-29 15:07:22 +0200

    speed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var Typer={
 	text: null,
 	accessCountimer:null,
 	index:0, 
-	speed:2,
+	speed:0,
 	file:"", 
 	accessCount:0,
 	deniedCount:0, 
@@ -97,7 +97,7 @@ function replaceUrls(text) {
 	}
 }
 
-Typer.speed=3;
+Typer.speed=0;
 Typer.file="robinseibt.txt"; // add your own name here
 Typer.init();
  
