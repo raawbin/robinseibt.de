57402d6abf6ba72221434b9fc217f579e23deaca
rawbin <57212166+raawbin@users.noreply.github.com>
2022-09-16 08:21:06 +0200

    set speed to 10
    whatever that does

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var Typer={
 	text: null,
 	accessCountimer:null,
 	index:0, 
-	speed:0,
+	speed:10,
 	file:"", 
 	accessCount:0,
 	deniedCount:0, 
@@ -97,7 +97,7 @@ function replaceUrls(text) {
 	}
 }
 
-Typer.speed=0;
+Typer.speed=10;
 Typer.file="robinseibt.txt"; // add your own name here
 Typer.init();
  
